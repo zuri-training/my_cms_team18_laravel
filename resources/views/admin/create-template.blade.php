e7eac47ee9e37a5652c0429125340f113d91c57d
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-07-28 18:59:44 +0100

    added admin create & edit template

---
+++ resources/views/admin/create-template.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Add New Template') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{route('original.template.store')}}">
+                            @csrf
+
+                            @include('includes.status')
+
+                            <div class="row mb-3">
+                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text"
+                                        class="form-control @error('name') is-invalid @enderror" name="name"
+                                        value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="content"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Template') }}</label>
+
+                                <div class="col-md-6">
+                                    <textarea id="content" type="content"
+                                        class="form-control @error('content') is-invalid @enderror" name="content"
+                                         required autocomplete="content" rows="50" cols="20">{{ old('content') }}</textarea>
+
+                                    @error('content')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Add Template') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
