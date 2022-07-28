019a0cc2a9961ac2ca557b9779161b3150589ec2
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-07-28 18:56:49 +0100

    Updated home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        if(Auth::user()->type == 3):
+            return view('user.dashboard');
+        endif;
+        
+        return view('admin.dashboard');
+    }
+}
