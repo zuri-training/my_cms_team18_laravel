9a2e7424c54b0ea1618b879ea72e379ac1c4e24f
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-07-28 19:01:22 +0100

    added extra navigation link for admin

---
+++ resources/views/includes/adminnav.blade.php
@@ -0,0 +1,2 @@
+<a class="dropdown-item" href="{{ route('original.template.index') }}">View Templates</a>
+<a class="dropdown-item" href="{{ route('original.template.create') }}">Add New Template</a>
